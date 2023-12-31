be74b42995b986b212b664b814c0098b3494174a
EHAB3x <92524624+EHAB3x@users.noreply.github.com>
2023-12-31 20:33:26 +0200

    Add Problem J

---
+++ J.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+
+    
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void printPrimesUpToN(int N) {
+    for (int i = 2; i <= N; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int number;
+    cin >> number;
+
+    printPrimesUpToN(number);
+
+    return 0;
+}
