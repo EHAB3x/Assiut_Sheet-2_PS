3c2077e06f1c32e1ad5bb8b402f0edaa8c0f9c34
EHAB3x <92524624+EHAB3x@users.noreply.github.com>
2024-01-05 19:49:44 +0200

    Add Problem V

---
+++ Sheet_2_Loops/V.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cin >> number;
+
+    for (int i = 1; i <= number * 4; i++)
+    {
+            if(i % 4 == 0){
+                cout << "PUM" << endl;
+            }else{
+                cout << i << " ";
+            }
+    }
+    
+    return 0;
+}
